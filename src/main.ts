29a053284b13222f8857ce6529c07b0aa707addb
Chiayung Luk <chiayung.luk@gmail.com>
2022-11-13 08:36:30 +0000

    refactor: update config of default port

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { config } from 'dotenv';
 import AppModule from './app.module';
-
-config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 }
 bootstrap();
